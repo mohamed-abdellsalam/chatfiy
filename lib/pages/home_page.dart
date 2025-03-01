7663690f3de1d6a090ea0ca1a33c0a8f78e2905b
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2025-03-01 16:39:24 +0200

    Add MyDrawer component and refactor HomePage; implement logout functionality

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,8 @@
-import 'package:chatify/auth/auth_service.dart';
+import 'package:chatify/components/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-  void logout() {
-    final auth = AuthService();
-    auth.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +11,16 @@ class HomePage extends StatelessWidget {
         title: const Text(
           'Home',
         ),
-        actions: [
-          IconButton(
-            onPressed: logout,
-            icon: const Icon(
-              Icons.logout,
-            ),
-          )
-        ],
+        // actions: [
+        //   IconButton(
+        //     onPressed: logout,
+        //     icon: const Icon(
+        //       Icons.logout,
+        //     ),
+        //   ),
+        // ],
       ),
+      drawer: const MyDrawer(),
     );
   }
 }
